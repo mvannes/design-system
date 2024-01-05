5f4fdf1d20fa97741ea0219b04e356828b97f3b0
Michael van Nes <m.f.van.nes@gmail.com>
2024-01-05 22:55:14 +0100

    Input

--- .storybook/stories/Input.stories.ts
+++ .storybook/stories/Input.stories.ts
@@ -1,40 +1,32 @@
 import type { StoryObj, Meta } from '@storybook/html';
 
-
-interface SelectArgs {
+interface InputArgs {
   label: string;
+  placeholder: string;
   variant: 'primary' | 'secondary' | 'accent';
-  amountOfOptions: number;
   error: boolean;
 }
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
-  title: 'Select',
+  title: 'Input',
   tags: ['autodocs'],
   render: (args) => {
     const variant = `form-${args.variant}`;
     const errored = args.error ? 'error' : '';
-    const options = [];
 
-    for (let i =0; i < args.amountOfOptions; i++) {
-      options.push(`
-        <option value="${i}">${args.label} ${i}</option>`)
-    }
     return `
     <div class="form ${variant}">
-        <div class="input-holder select-holder ${errored}">
-          <label for="select">Select</label>
-          <select name="select" id="select">
-            ${options.join('\n')}
-          </select>
-          <i class="select-icon"></i>
+        <div class="input-holder ${errored}">
+          <label for="input">${args.label}</label>
+          <input type="text" name="input" id="input" placeholder="${args.placeholder}">
         </div>
     </div>   
     `
   },
   argTypes: {
     label: { control: 'text' },
+    placeholder: { control: 'text' },
     variant: {
       options: [
         'primary',
@@ -43,25 +35,22 @@ const meta = {
       ],
       control: 'radio'
     },
-    amountOfOptions: {
-      control: 'number',
-    },
     error: {
       control: 'boolean'
     }
   },
-} satisfies Meta<SelectArgs>;
+} satisfies Meta<InputArgs>;
 
 export default meta;
 
-type Story = StoryObj<SelectArgs>;
+type Story = StoryObj<InputArgs>;
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Input: Story = {
   args: {
     label: 'Label',
+    placeholder: 'Placeholder',
     variant: 'primary',
-    amountOfOptions: 3,
     error: false,
   },
 };
